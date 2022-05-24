c154bee75d4e4a515eb6a4b21cb93b5dec38f54f
Karel Král <karelkral@google.com>
2022-05-24 14:54:25 +0200

    [squash] improve communication docstring

--- scaaml/capture/communication.py
+++ scaaml/capture/communication.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""The communication."""
+"""Communication protocols between the capturing host and the chip."""
 
 from abc import ABC, abstractmethod
 
