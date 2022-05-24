3036f306ffc585c0f80e293016747147f0434b5c
Karel Král <karelkral@google.com>
2022-05-24 15:43:16 +0200

    Remove unused parameter and improve docstring
    Remove part of docstring that is specific to a subclass.

--- scaaml/capture/scope/scope_base.py
+++ scaaml/capture/scope/scope_base.py
@@ -19,27 +19,13 @@ from abc import ABC, abstractmethod
 
 class AbstractSScope(ABC):
     """Scope context manager."""
-    def __init__(self, samples: int, offset: int, **_):
+    def __init__(self, samples: int, offset: int):
         """Create scope context.
 
         Args:
           samples: How samples to capture (length of the capture).
           offset: Number of samples to wait after trigger event occurred before
             starting recording data.
-          _: CWScope is expected to be initialized using the capture_info
-            dictionary which may contain extra keys (additional information
-            about the capture; the capture_info dictionary is saved in the
-            info file of the dataset). Thus we can ignore the rest of keyword
-            arguments.
-
-        Expected use:
-          capture_info = {
-              'samples': samples,
-              'offset': offset,
-              'other_information': 'Can also be present.',
-          }
-          with CWScope(**capture_info) as scope:
-              # Use the scope object.
         """
         self._scope = None
         self._samples = samples
