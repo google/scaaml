60f3f6fe842e38b71ad12da16eacf5ddb6c62cca
Jean-Michel Picod <jmichel@google.com>
2022-05-27 16:28:43 +0200

    Fix file docstring

--- scaaml/utils.py
+++ scaaml/utils.py
@@ -11,8 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Utils common to various SCAAML components"""
 
-"Utils common to various SCAAML components"
 from tqdm.auto import tqdm
 import tensorflow as tf
 import numpy as np
