021581b29ce47ece25fe15b90dcb67920ca31c3c
Karel Král <karelkral@google.com>
2022-07-08 12:16:16 +0200

    DO NOT SUBMIT test also changing an older file

--- scaaml/io/dataset.py
+++ scaaml/io/dataset.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Build and load tensorFlow dataset Record wrapper"""
+"""Build and load tensorFlow dataset Record wraper"""
 
 import copy
 import math
