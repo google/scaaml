96f4bb864c5c6461ca64cd581c0aea6f56d93d69
Karel Král <karelkral@google.com>
2022-08-30 13:38:56 +0200

    Group imports from scaaml

--- scaaml/stats/print_stats.py
+++ scaaml/stats/print_stats.py
@@ -23,10 +23,11 @@ from typing import Dict, List, Set
 from tabulate import tabulate
 from tqdm import tqdm
 
+from scaaml_dataset.io import ExampleIterator
+
 from scaaml.io import Dataset
 from scaaml.stats.ap_counter import APCounter
 from scaaml.stats.ap_checker import APChecker
-from scaaml_dataset.io import ExampleIterator
 from scaaml.stats.trace_stddev_of_stat import STDDEVofSTATofTraces
 from scaaml.stats.trace_stddev_of_stat import STDDEVofAVGofTraces
 from scaaml.stats.trace_stddev_of_stat import STDDEVofMAXofTraces
