97fc8c9997804140f36a40fb0683b5b5dddee6af
Karel Král <karelkral@google.com>
2023-12-04 12:46:09 +0100

    Fix typehint

--- scaaml/utils.py
+++ scaaml/utils.py
@@ -13,15 +13,17 @@
 # limitations under the License.
 """Utils common to various SCAAML components"""
 
-from tqdm.auto import tqdm
-import tensorflow as tf
-import numpy as np
-import chipwhisperer as cw
-from glob import glob
 from multiprocessing import Pool
-import time
 from random import randint
+import time
+from typing import Literal
+
+from glob import glob
 from termcolor import cprint
+from tqdm.auto import tqdm
+import chipwhisperer as cw
+import numpy as np
+import tensorflow as tf
 
 
 def pretty_hex(val):
@@ -132,10 +134,9 @@ def display_config(config_name, config):
     cprint(f"[{config_name}]", "magenta")
     cnt = 1
     for k, v in config.items():
+        color: Literal["yellow", "cyan"] = "yellow"
         if cnt % 2:
             color = "cyan"
-        else:
-            color = "yellow"
         cprint(f"{k}:{v}", color)
         cnt += 1
 
