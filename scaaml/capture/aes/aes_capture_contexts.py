8a4c8389d38d1f0a33d5f09a01d3813a5a440726
Karel Kral <karel@star-lab.xyz>
2023-12-06 14:54:02 +0000

    [squash] get default value is None

--- scaaml/capture/aes/aes_capture_contexts.py
+++ scaaml/capture/aes/aes_capture_contexts.py
@@ -266,8 +266,8 @@ def _capture(scope_class, capture_info: Dict[str, Any], chip_id: int,
             dataset.write_config()
 
             assert lecroy.scope is not None
-            with DefaultCWScope(capture_info.get("cw_scope_serial_number",
-                                                 None)) as default_cwscope:
+            with DefaultCWScope(capture_info.get(
+                    "cw_scope_serial_number")) as default_cwscope:
                 _control_communication_and_capture(
                     chip_id=chip_id,
                     cwscope=default_cwscope,
@@ -281,8 +281,8 @@ def _capture(scope_class, capture_info: Dict[str, Any], chip_id: int,
     if scope_class == PicoScope:
         with PicoScope(**capture_info) as picoscope:
             assert picoscope.scope is not None
-            with DefaultCWScope(capture_info.get("cw_scope_serial_number",
-                                                 None)) as default_cwscope:
+            with DefaultCWScope(capture_info.get(
+                    "cw_scope_serial_number")) as default_cwscope:
                 _control_communication_and_capture(
                     chip_id=chip_id,
                     cwscope=default_cwscope,
