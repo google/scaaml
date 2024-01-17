e685b04f236de29be6edd42724938a6f1f974233
Luca Tomasko <lucatomasko@google.com>
2024-01-17 13:34:57 +0000

    [squash] Add docstring to init file

--- scaaml/capture/input_generators/__init__.py
+++ scaaml/capture/input_generators/__init__.py
@@ -11,5 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Attack point generators"""
 
 from scaaml.capture.input_generators.input_generators import balanced_generator, unrestricted_generator
