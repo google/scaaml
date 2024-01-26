c8979b2f8ab17b50dca7604afe1af7a9e16f4e5f
LucaTomasko <89468158+LucaTomasko@users.noreply.github.com>
2024-01-26 15:22:53 +0100

    Add attack point iterator balanced generator (#193)
    * Add attack point iterator balanced generator
    
    Part of issue #196

--- tests/capture/input_generators/test_attack_point_iterator.py
+++ tests/capture/input_generators/test_attack_point_iterator.py
@@ -12,7 +12,7 @@ def attack_point_iterator_constants(values):
     assert output == values
 
 
-def test_attack_point_itarattor_no_legal_operation():
+def test_attack_point_iterator_no_legal_operation():
     values = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
               [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]]
     input = {"operation": "NONE", "name": "key", "values": values}
@@ -57,3 +57,75 @@ def test_repeated_iteration_constants():
               [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]]
     config = {"operation": "constants", "name": "key", "values": values}
     repeated_iteration(config)
+
+
+def test_attack_point_iterator_balanced_generator():
+    config = {"operation": "balanced_generator", "name": "key", "length": 16}
+    output = list(iter(AttackPointIterator(config)))
+    assert len(output) == 256
+
+
+def test_attack_point_iterator_balanced_generator_all_kwargs():
+    config = {
+        "operation": "balanced_generator",
+        "name": "key",
+        "length": 16,
+        "bunches": 2,
+        "elements": 3
+    }
+    output = list(iter(AttackPointIterator(config)))
+    assert len(output) == config["bunches"] * config["elements"]
+
+
+def test_attack_point_iterator_unrestricted_generator():
+    config = {
+        "operation": "unrestricted_generator",
+        "name": "key",
+        "length": 16
+    }
+    output = list(iter(AttackPointIterator(config)))
+    assert len(output) == 256
+
+
+def test_attack_point_iterator_balanced_generator_all_args():
+    config = {
+        "operation": "unrestricted_generator",
+        "name": "key",
+        "length": 16,
+        "bunches": 2,
+        "elements": 3
+    }
+    output = list(iter(AttackPointIterator(config)))
+    assert len(output) == config["bunches"] * config["elements"]
+
+
+def test_attack_point_iterator_balanced_generator_len():
+    config = {"operation": "balanced_generator", "name": "key", "length": 16}
+    output = AttackPointIterator(config)
+    assert len(output) == 256
+
+
+def test_attack_point_iterator_balanced_generator_all_args_len():
+    config = {
+        "operation": "balanced_generator",
+        "name": "key",
+        "length": 16,
+        "bunches": 2,
+        "elements": 3
+    }
+    output = list(iter(AttackPointIterator(config)))
+    assert len(output) == config["bunches"] * config["elements"]
+    assert len(output) == len(AttackPointIterator(config))
+
+
+def test_attack_point_iterator_unrestricted_generator_all_args_len():
+    config = {
+        "operation": "unrestricted_generator",
+        "name": "key",
+        "length": 16,
+        "bunches": 2,
+        "elements": 3
+    }
+    output = list(iter(AttackPointIterator(config)))
+    assert len(output) == config["bunches"] * config["elements"]
+    assert len(output) == len(AttackPointIterator(config))
