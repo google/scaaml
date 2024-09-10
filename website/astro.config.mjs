6b5809014808d4622a06cc2a9c283105358f3a80
wsxrdv <111074929+wsxrdv@users.noreply.github.com>
2024-09-10 18:17:36 +0200

    Fix guides slug (#278)

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -15,7 +15,7 @@ export default defineConfig({
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Example Guide', slug: 'guides/capture' },
 					],
 				},
 				{
