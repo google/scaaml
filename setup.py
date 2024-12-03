50e6b4de9a2e0b9654935502415e0a4009b7d20d
kralka <karelkral@google.com>
2024-12-03 16:47:40 +0100

    Add GitHub releasing (#319)

--- setup.py
+++ setup.py
@@ -11,53 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Setup script."""
-from setuptools import find_packages
+"""Compatibility with legacy builds.
+"""
+
 from setuptools import setup
-from time import time
 
-with open("README.md", encoding="utf-8") as readme_file:
-    long_description = readme_file.read()
-version = f"2.0.1r{int(time())}"
-
-setup(
-    name="scaaml",
-    python_requires=">=3.9",
-    version=version,
-    description="Side Channel Attack Assisted with Machine Learning",
-    long_description=long_description,
-    author="Elie Bursztein",
-    author_email="scaaml@google.com",
-    url="https://github.com/google/scaaml",
-    license="Apache License 2.0",
-    install_requires=[
-        "colorama",
-        "termcolor",
-        "tqdm",
-        "pandas",
-        "pytest",
-        "numpy",
-        "tabulate",
-        "matplotlib",
-        "Pillow",
-        "tensorflow>=2.2.0",
-        "pygments",
-        "pyvisa",
-        "pyvisa-py",
-        "chipwhisperer",
-        "scipy",
-        "semver",
-    ],
-    package_data={"": ["*.pickle"]},
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Framework :: Jupyter",
-        "License :: OSI Approved :: Apache Software License",
-        "Intended Audience :: Science/Research",
-        "Programming Language :: Python :: 3",
-        "Topic :: Security :: Cryptography",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-    ],
-    packages=find_packages(),
-)
+setup()
