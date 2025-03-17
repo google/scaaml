b9d8296573781e9f9e8e0a5db8670501e04ef833
Karel <karel@star-lab.xyz>
2025-03-17 15:12:57 +0000

    Allow configuring resolution for PicoScope

--- scaaml/capture/scope/picoscope.py
+++ scaaml/capture/scope/picoscope.py
@@ -24,61 +24,125 @@ from scaaml.capture.scope.ps6424e import Pico6424E as PicoScope6424E
 
 class PicoScope(AbstractSScope):
     """Scope context manager."""
-    CHANNEL_T: TypeAlias = Literal["A", "B", "C", "D", "E", "F", "G", "H",
-                                   "PORT0", "PORT1"]
-    COUPLING_T: TypeAlias = Literal["AC", "DC", "DC50"]
-    ATTENUATION_T: TypeAlias = Literal["1:1", "1:10"]
-    BW_LIMIT_T: TypeAlias = Literal["PICO_BW_FULL", "PICO_BW_20MHZ",
-                                    "PICO_BW_200MHZ"]
+    CHANNEL_T: TypeAlias = Literal[
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "PORT0",
+        "PORT1",
+    ]
+    COUPLING_T: TypeAlias = Literal[
+        "AC",
+        "DC",
+        "DC50",
+    ]
+    ATTENUATION_T: TypeAlias = Literal[
+        "1:1",
+        "1:10",
+    ]
+    BW_LIMIT_T: TypeAlias = Literal[
+        "PICO_BW_FULL",
+        "PICO_BW_20MHZ",
+        "PICO_BW_200MHZ",
+    ]
+    RESOLUTION_T: TypeAlias = Literal[
+        "PICO_DR_8BIT",
+        "PICO_DR_10BIT",
+        "PICO_DR_12BIT",
+        "PICO_DR_14BIT",
+        "PICO_DR_15BIT",
+        "PICO_DR_16BIT",
+    ]
 
-    def __init__(self, *, samples: int, sample_rate: float, offset: int,
-                 trace_channel: CHANNEL_T, trace_probe_range: float,
-                 trace_coupling: COUPLING_T, trace_attenuation: ATTENUATION_T,
-                 trace_bw_limit: BW_LIMIT_T, trace_ignore_overflow: bool,
-                 trigger_channel: CHANNEL_T, trigger_hysteresis: Optional[str],
-                 trigger_pin: Optional[int], trigger_range: float,
-                 trigger_level: float, trigger_coupling: COUPLING_T,
-                 **_: Any) -> None:
+    def __init__(
+        self,
+        *,
+        samples: int,
+        sample_rate: float,
+        offset: int,
+        trace_channel: CHANNEL_T,
+        trace_probe_range: float,
+        trace_coupling: COUPLING_T,
+        trace_attenuation: ATTENUATION_T,
+        trace_bw_limit: BW_LIMIT_T,
+        trace_ignore_overflow: bool,
+        trigger_channel: CHANNEL_T,
+        trigger_hysteresis: Optional[str],
+        trigger_pin: Optional[int],
+        trigger_range: float,
+        trigger_level: float,
+        trigger_coupling: COUPLING_T,
+        resolution: RESOLUTION_T = "PICO_DR_10BIT",
+        **_: Any,
+    ) -> None:
         """Create scope context.
 
         Args:
+
           samples (int): How many points to sample (length of the capture).
+
           sample_rate (float): How many samples per second to take in Hz. The
-            setting will be the closest PicoScope sampling rate that is at
-            least sample_rate.
+          setting will be the closest PicoScope sampling rate that is at least
+          sample_rate.
+
           offset (int): How many samples are discarded between the trigger
-            event and the start of the trace.
+          event and the start of the trace.
+
           trace_channel (str): Which channel to use for the signal.
+
           trace_probe_range (float): Should be in [0.02, 0.05, 0.1, 0.2, 0.5,
-            1.0, 2.0, 5.0, 10.0, 20.0, 50.0] (in V).
+          1.0, 2.0, 5.0, 10.0, 20.0, 50.0] (in V).
+
           trace_coupling (str): Which coupling to use, one of AC, DC, DC50 (DC
-            with 50 Ohm).
-          trace_attenuation (str): Which attenuation to use, either 1:1 or 1:10.
-            For BNC-SMA pigtail use "1:1", for oscilloscope passive probe
-            use "1:10".
+          with 50 Ohm).
+
+          trace_attenuation (str): Which attenuation to use, either 1:1 or
+          1:10.  For BNC-SMA pigtail use "1:1", for oscilloscope passive probe
+          use "1:10".
+
           trace_bw_limit (str): Bandwidth limit. Filter out too high
-            frequencies (can prevent overflows in trace).
+          frequencies (can prevent overflows in trace).
+
           trace_ignore_overflow (bool): If True then we ignore trace overflow.
-            Otherwise PicoScope6424E.capture returns True (timeouts).
+          Otherwise PicoScope6424E.capture returns True (timeouts).
+
           trigger_channel (str): Which channel to use. Values are: "A", "B",
-            "C", "D", (if the scope has those "E", "F", "G", "H"). If the
-            scope has digital ports then "PORT0" and "PORT1".
+          "C", "D", (if the scope has those "E", "F", "G", "H"). If the scope
+          has digital ports then "PORT0" and "PORT1".
+
           trigger_hysteresis (Optional[str]): None if the trigger is analog.
-            Otherwise one of PICO_VERY_HIGH_400MV, PICO_HIGH_200MV,
-            PICO_NORMAL_100MV, PICO_LOW_50MV.
+          Otherwise one of PICO_VERY_HIGH_400MV, PICO_HIGH_200MV,
+          PICO_NORMAL_100MV, PICO_LOW_50MV.
+
           trigger_pin (Optional[int]): Only when using MSO port as
-            trigger_channel. Which pin to trigger on.
-          trigger_range (float): Should be in [0.02, 0.05, 0.1, 0.2, 0.5,
-            1.0, 2.0, 5.0, 10.0, 20.0, 50.0] (in V). Analog trigger only.
+          trigger_channel. Which pin to trigger on.
+
+          trigger_range (float): Should be in [0.02, 0.05, 0.1, 0.2, 0.5, 1.0,
+          2.0, 5.0, 10.0, 20.0, 50.0] (in V). Analog trigger only.
+
           trigger_level (float): When to trigger (in V).
+
           trigger_coupling (str): Which coupling to use, one of AC, DC, DC50
-            (DC with 50 Ohm). Analog trigger only.
+          (DC with 50 Ohm). Analog trigger only.
+
+          resolution (RESOLUTION_T): The resolution of the given PicoScope. For
+          PicoScope6424E PICO_DR_8BIT, PICO_DR_10BIT, and PICO_DR_12BIT are
+          supported. But if too high sample_rate is chosen
+          PICO_CHANNEL_COMBINATION_NOT_VALID_IN_THIS_RESOLUTION is raised.
+
           _: PicoScope is expected to be initialized using capture_info
-            dictionary, this parameter allows to have additional information
-            there and initialize as PicoScope(**capture_info).
+          dictionary, this parameter allows to have additional information
+          there and initialize as PicoScope(**capture_info).
         """
         super().__init__(samples=samples, offset=offset)
         self._sample_rate = sample_rate
+
+        self._resolution: RESOLUTION_T = resolution
 
         # Trace settings
         self._trace_channel: PicoScope.CHANNEL_T = trace_channel
@@ -111,6 +175,7 @@ class PicoScope(AbstractSScope):
 
         # Connect to the oscilloscope.
         self._scope = PicoScope6424E()
+        self._scope.set_resolution(self._resolution)
         self._scope.con()
 
         # Trace channel settings.
